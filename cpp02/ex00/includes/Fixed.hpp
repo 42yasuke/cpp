7d7cf2ce90ff0e4967d21837017efc0a4b9dbc04
Jose RALPH <jralph@paul-f3Ar3s4.clusters.42paris.fr>
2024-02-06 17:03:14 +0100

    cpp02 ex00 start

---
+++ cpp02/ex00/includes/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 16:44:14 by jralph            #+#    #+#             */
+/*   Updated: 2024/02/06 16:47:55 by jralph           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+	private:
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(Fixed &const fix);
+};
+
+#endif
