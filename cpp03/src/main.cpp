398e34e61e4f5e5fe9fa8ca7c4652e525824381a
Jose RALPH <jralph@bess-f1r5s10.clusters.42paris.fr>
2024-02-08 18:24:14 +0100

    cpp03 ex00

---
+++ cpp03/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 17:48:12 by jralph            #+#    #+#             */
+/*   Updated: 2024/02/08 18:22:44 by jralph           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	a;
+	ClapTrap	b("Morgan");
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	b.takeDamage(3);
+	std::cout << b << std::endl;
+	b.takeDamage(30);
+	std::cout << b << std::endl;
+	b.beRepaired(3);
+	std::cout << b << std::endl;
+	b.attack("Eric");
+	b.attack("Eric");
+	std::cout << b << std::endl;
+	b.attack("Eric");
+	std::cout << b << std::endl;
+	b.beRepaired(6);
+	std::cout << b << std::endl;
+	b.attack("Eric");
+}
