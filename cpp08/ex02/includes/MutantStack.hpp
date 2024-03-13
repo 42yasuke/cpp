864d55b94d411bdb51fe66b4a0d09c238f83d716
42yasuke <j.ralph1998@gmail.com>
2024-03-13 17:19:11 +0100

    cpp08 done

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 16:03:03 by jose              #+#    #+#             */
-/*   Updated: 2024/03/13 16:52:10 by jose             ###   ########.fr       */
+/*   Updated: 2024/03/13 17:05:23 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack<T>() {};
-		MutantStack(const MutantStack& copy) : std::stack<T>(copy){*this=copy;};
+		MutantStack<T>() {}
+		MutantStack(const MutantStack& copy) : std::stack<T>(copy){}
 		virtual ~MutantStack<T>() {}
 		MutantStack<T> &operator=(const MutantStack<T> &copy)
 		{
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 21:15:10 by jralph            #+#    #+#             */
-/*   Updated: 2024/03/13 16:59:28 by jose             ###   ########.fr       */
+/*   Updated: 2024/03/13 17:18:31 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,17 @@ int	main(void)
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "-----------------COPY CONSTRUCTOR--------------------" << std::endl;
+	MutantStack<int> mstack2(mstack);
+	std::cout << "mstack2 size: " <<mstack2.size() << std::endl;
+	it = mstack2.begin();
+	ite = mstack2.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
 	return 0;
 }
