cb84883874886d9b1765ef31fd9c975998e89eaf
42yasuke <j.ralph1998@gmail.com>
2024-04-08 19:21:58 +0200

    cpp06 done

--- cpp06/ex01/includes/Serializer.hpp
+++ cpp06/ex01/includes/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 12:48:03 by jose              #+#    #+#             */
-/*   Updated: 2024/02/26 13:41:20 by jose             ###   ########.fr       */
+/*   Updated: 2024/04/08 19:06:29 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,17 @@ typedef struct data_t
 
 class Serializer
 {
+	private:
+		Serializer();
+		Serializer(Serializer const &sr);
+		Serializer	&operator=(Serializer const &sr);
 	public:
+		~Serializer();
 		static uintptr_t	serialize(Data* ptr);
 		static Data	*deserialize(uintptr_t raw);
 };
+
+std::ostream &operator<<(std::ostream &os, Serializer const &sr);
 
 #endif
 
--- cpp06/ex01/src/Serializer.cpp
+++ cpp06/ex01/src/Serializer.cpp
@@ -6,11 +6,27 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 12:31:24 by jose              #+#    #+#             */
-/*   Updated: 2024/02/26 13:17:12 by jose             ###   ########.fr       */
+/*   Updated: 2024/04/08 19:11:42 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+
+Serializer::Serializer()
+{
+}
+
+Serializer::~Serializer()
+{
+}
+Serializer::Serializer(Serializer const &sr)
+{
+	(void)sr;
+}
+Serializer	&Serializer::operator=(Serializer const &sr)
+{
+	return ((void)sr, *this);
+}
 
 uintptr_t	Serializer::serialize(Data *ptr)
 {
@@ -20,4 +36,9 @@ uintptr_t	Serializer::serialize(Data *ptr)
 Data	*Serializer::deserialize(uintptr_t raw)
 {
 	return (reinterpret_cast<Data*>(raw));
+}
+
+std::ostream &operator<<(std::ostream &os, Serializer const &sr)
+{
+	return ((void)sr, os);
 }
